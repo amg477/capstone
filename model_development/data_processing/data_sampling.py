6669e6916bf0b0deca5cc88fdb8146ee8fe734ab
Anna <amg477@georgetown.edu>
2025-10-22 21:23:39 -0400

    Sampled dataset with 100K rows output.

--- model_development/data_processing/data_sampling.py
+++ model_development/data_processing/data_sampling.py
@@ -21,8 +21,9 @@ import pandas as pd
 # Config (paths + defaults)
 # ---------------------------------------------------------------------
 ROOT = Path("/Users/annaglass/capstone/capstone")
-INPUT_PATH  = ROOT / "data" / "processed" / "processed_data.parquet"
-OUTPUT_PATH = ROOT / "data" / "processed" / "final_dataset_sampled.parquet"
+RAW_DIR       = ROOT / "data_storage" / "raw_data"
+INPUT_PATH  = ROOT / "data_storage" / "processed_data" / "processed_data.parquet"
+OUTPUT_PATH = ROOT / "data" / "processed" / "sampled_data.parquet"
 
 DEFAULT_TARGET_N = 100_000
 DEFAULT_SEED     = 2025
